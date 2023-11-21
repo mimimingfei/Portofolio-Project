23bace8c894414e95a85cc0c056df21485e96b6d
mingfei <mingfei@CPC-mwang-NL26M>
2023-11-21 15:11:22 +0000

    task 5 done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.get('/api/topics', getAllTopics);
 app.get('/api', getAllEndpoints);
 app.get('/api/articles/:article_id',getArticleById)
-
+app.get('/api/articles',getAllArticles)
 
 app.all("*", handle404)
 app.use(handlePsqErrors)
