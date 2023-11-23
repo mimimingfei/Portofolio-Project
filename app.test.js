203d6cdba0af079fc920f496a0fb0b0a1993c00a
mingfei <mingfei@CPC-mwang-NL26M>
2023-11-23 15:33:20 +0000

    -

--- app.test.js
+++ app.test.js
@@ -189,7 +189,7 @@ describe("POST /api/articles/:article_id/comments", () => {
                 expect(Comment.article_id).toBe(1)
                 expect(Comment.author).toBe('butter_bridge')
             })
-
+        })
     test("400: Bad request, missing part of input comment data ", () => {
         const newComment = { comment: "test" };
         return request(app)
@@ -357,6 +357,7 @@ describe("GET /api/users", () => {
                 })
             })
     })
+})
 
-})
-})
+
+
